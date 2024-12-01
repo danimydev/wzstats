a4e85a346e0623847d1f87488306eae7f0c89ee6
Daniel Madrid <danimydev@gmail.com>
2024-12-01 15:42:26 -0500

    fix: removes auto request cron job

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,3 @@ import "./bot/index.ts";
 Deno.serve(() => {
   return new Response("Hello World");
 });
-
-Deno.cron("Run once every 5 minutes", { minute: { every: 5 } }, async () => {
-  const res = await fetch("http://0.0.0.0:8000/");
-  console.log(res);
-});
