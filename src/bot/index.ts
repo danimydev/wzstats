ff255415a6774b5947da49738d0328397ab7787b
Daniel Madrid <danimydev@gmail.com>
2024-12-02 19:55:02 -0500

    fix: ingest cron job

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,15 +2,11 @@ import { Client, Events, GatewayIntentBits } from "npm:discord.js";
 
 import config from "./config.ts";
 
-import metaCommand, {
-  buildFormattedWeapons,
-  buildWeaponEmbeds,
-} from "./commands/meta.ts";
+import metaCommand from "./commands/meta.ts";
 import subscribeCommand from "./commands/subscribe.ts";
 import unsubscribeCommand from "./commands/unsubscribe.ts";
-import * as channelsRepository from "./channels/repository.ts";
 
-const client = new Client({
+export const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
@@ -45,55 +41,3 @@ client.once(Events.ClientReady, (readyClient) => {
 });
 
 client.login(config.TOKEN);
-
-Deno.cron("Ingest from wzstats", { hour: { every: 12 } }, async () => {
-  console.log("Running cron job...");
-
-  const ingestCommand = new Deno.Command(Deno.execPath(), {
-    args: ["task", "ingest"],
-    stdout: "piped",
-    stdin: "piped",
-    stderr: "piped",
-  });
-
-  const childProccess = ingestCommand.spawn();
-  const { code, stderr, stdout, success } = await childProccess.output();
-
-  const td = new TextDecoder();
-
-  if (code === 0 && success) {
-    console.log("Cron job ran successfully!");
-    console.log(td.decode(stdout));
-  } else {
-    console.log("Cron job ran with errors!");
-    console.log(td.decode(stderr));
-  }
-});
-
-Deno.cron("Daily meta post", { hour: { every: 23 } }, async () => {
-  console.log("Running cron job...");
-
-  if (client.isReady()) {
-    const channelsId = (await channelsRepository.getChannels()).map((c) =>
-      c.id
-    );
-    if (config.DEV_CHANNEL_ID) {
-      channelsId.push(config.DEV_CHANNEL_ID);
-    }
-    const channelsPromise = channelsId.map((channelId) =>
-      client.channels.fetch(channelId)
-    );
-    const textChannels = (await Promise.all(channelsPromise))
-      .filter((channel) => channel !== null && channel.type === 0);
-    const formattedWeapons = await buildFormattedWeapons();
-    const messagesPromise = textChannels.map((channel) =>
-      channel.send({
-        content: "Warzone Meta",
-        embeds: buildWeaponEmbeds(formattedWeapons.toSpliced(6)),
-      })
-    );
-    await Promise.all(messagesPromise);
-  }
-
-  return console.log("Cron job ran successfully!");
-});
---
+++ src/wzstats/ingest.ts
@@ -0,0 +1,29 @@
+import config from "./config.ts";
+
+export default async function ingest() {
+  console.log("Running ingest...");
+  console.log(
+    `Fetching weapons and tier lists from ${config.WEAPONS_AND_TIER_LISTS_URL}`,
+  );
+
+  const response = await fetch(config.WEAPONS_AND_TIER_LISTS_URL);
+  const data = await response.json();
+
+  console.log(`Writing weapons file at ${config.WEAPONS_FILE_PATH}`);
+
+  const ingestWeapons = Deno.writeTextFile(
+    config.WEAPONS_FILE_PATH,
+    JSON.stringify(data.weapons),
+  );
+
+  console.log(`Writing tier lists file at ${config.TIER_LISTS_FILE_PATH}`);
+
+  const ingestTierLists = Deno.writeTextFile(
+    config.TIER_LISTS_FILE_PATH,
+    JSON.stringify(data.wzStatsTierList),
+  );
+
+  await Promise.all([ingestTierLists, ingestWeapons]);
+
+  console.log("Successfully ran ingest!");
+}
