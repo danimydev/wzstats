e89a91c9ddc7f766b68abc9cbaf45b12180c4421
Daniel Madrid <dani_my98@mac-mini-m4.local>
2025-10-11 12:45:59 -0500

    chore(api): use native deno serve handler.

---
+++ src/api/handler.ts
@@ -0,0 +1,59 @@
+import * as channelsRepository from "../repositories/channel.ts";
+import * as tierListsRepository from "../repositories/tier-list.ts";
+import * as Utils from "./utils.ts";
+import config from "./config.ts";
+
+const handler: Deno.ServeHandler<Deno.NetAddr> = async (req) => {
+  const { method, headers, url } = req;
+  const { pathname } = new URL(url);
+
+  if (method === "GET" && pathname === "/healthcheck") {
+    return new Response("OK", { status: 200 });
+  }
+
+  if (pathname === "/channels") {
+    const adminKey = headers.get("x-admin-key");
+
+    if (config.ADMIN_KEY !== adminKey) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    if (method === "GET") {
+      const channels = await channelsRepository.getChannels();
+      return new Response(Utils.stringify(channels), {
+        status: 200,
+        headers: { "content-type": "application/json; charset=utf-8" },
+      });
+    }
+
+    if (method === "DELETE") {
+      await channelsRepository.deleteChannels();
+      return new Response("OK", { status: 200 });
+    }
+  }
+
+  if (pathname === "/tier-lists") {
+    const adminKey = headers.get("x-admin-key");
+
+    if (config.ADMIN_KEY !== adminKey) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    if (method === "GET") {
+      const tierLists = await tierListsRepository.getTierLists();
+      return new Response(Utils.stringify(tierLists), {
+        status: 200,
+        headers: { "content-type": "application/json; charset=utf-8" },
+      });
+    }
+
+    if (method === "DELETE") {
+      await tierListsRepository.deleteTierLists();
+      return new Response("OK", { status: 200 });
+    }
+  }
+
+  return new Response("Not Found", { status: 404 });
+};
+
+export default handler;
