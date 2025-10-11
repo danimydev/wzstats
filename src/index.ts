027f65e047e96dcd8e7ab75a0c12c77278d2878f
Daniel Madrid <dani_my98@mac-mini-m4.local>
2025-10-11 12:51:08 -0500

    chore: use native handler instead of hono fetch.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import client from "./bot/client.ts";
 import config from "./bot/config.ts";
 import postDailyMeta from "./cron-jobs/post-daily-meta.ts";
 import ingest from "./cron-jobs/ingest.ts";
-import app from "./api/index.ts";
+import handler from "./api/handler.ts";
 
 client.login(config.TOKEN);
 
 Deno.cron("Ingest", { hour: { every: 1 } }, ingest);
 Deno.cron("Daily meta post", { hour: { exact: 0 } }, postDailyMeta);
 
-Deno.serve(app.fetch);
+Deno.serve(handler);
